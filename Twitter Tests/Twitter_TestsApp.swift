85c47a89d304f957969e7ea9e6f5c2cbd7592491
David Russell <ghostcode@me.com>
2021-07-01 17:53:07 -0400

    Initial Commit

---
+++ Twitter Tests/Twitter_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_TestsApp.swift
+//  Twitter Tests
+//
+//  Created by David Russell on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
