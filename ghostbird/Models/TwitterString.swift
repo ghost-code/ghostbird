a47d13b57afeece21aad0026c88ae686da0a0870
ghostcode <ghostcode@me.com>
2021-08-11 17:33:21 -0400

    Add TwitterString and TwitterStringEntity

---
+++ ghostbird/Models/TwitterString.swift
@@ -0,0 +1,62 @@
+//
+//  TextEntity.swift
+//  TextEntity
+//
+//  Created by David Russell on 8/6/21.
+//
+
+import SwiftUI
+
+struct TwitterString {
+
+    let entities: [TwitterStringEntity]
+    let string: String
+    let attributedString: NSAttributedString
+
+    private let font: UIFont = UIFont.systemFont(ofSize: 16)
+    private let elementColor = UIColor(.accentColor)
+
+    init(string: String, entities: [TwitterStringEntity], language: String?) {
+        let isLTR: Bool
+        if let language = language {
+            isLTR =  NSLocale.characterDirection(forLanguage: language) == .leftToRight
+        } else {
+            isLTR = true
+        }
+
+        self.string = string
+        self.entities = entities
+        self.attributedString = TwitterString.attributedString(for: string,
+                                                                  entities: entities,
+                                                                  font: font,
+                                                                  elementColor: elementColor,
+                                                                  isLTR: isLTR)
+    }
+
+    static func attributedString(for text: String,
+                                 entities: [TwitterStringEntity],
+                                 font: UIFont,
+                                 elementColor: UIColor,
+                                 isLTR: Bool) -> NSAttributedString {
+
+        let text = text + "\u{0000}"
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = isLTR ? .left : .right
+        paragraphStyle.lineBreakMode = .byWordWrapping
+
+        let attributes = [ NSAttributedString.Key.paragraphStyle: paragraphStyle,
+                           NSAttributedString.Key.foregroundColor: UIColor.label,
+                           NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16) ]
+
+        let attributedString = NSMutableAttributedString(string: text, attributes: attributes)
+
+        for entity in entities {
+            attributedString.addAttribute(NSAttributedString.Key.foregroundColor,
+                                          value: UIColor(named: "AccentColor")!,
+                                          range: entity.range)
+        }
+
+        return attributedString
+    }
+
+}
---
+++ ghostbird/Models/TwitterStringEntity.swift
@@ -0,0 +1,20 @@
+//
+//  TwitterStringEntity.swift
+//  TwitterStringEntity
+//
+//  Created by David Russell on 8/11/21.
+//
+
+import Foundation
+
+struct TwitterStringEntity {
+
+    enum EntityType {
+        case cashtag, hashtag, mention, url
+    }
+
+    var type: EntityType
+    var range: NSRange
+    var value: String
+
+}
